ef93177338ab28c19d09a1c4609d38a6f3810c41
HeligPfleigh <trantuan1892@gmail.com>
2023-01-13 22:15:11 +0700

    chore: replace ad test id by the real one

--- src/components/ads/config.ts
+++ src/components/ads/config.ts
@@ -7,25 +7,25 @@ export const adConfigs = {
   interstitialAdUnitId: __DEV__
     ? TestIds.INTERSTITIAL
     : Platform.select({
-        ios: 'ca-app-pub-xxxxxxxxxxxxx/yyyyyyyyyyyyyy',
+        ios: 'ca-app-pub-3548016532531500/3153137752',
         android: 'ca-app-pub-xxxxxxxxxxxxx/yyyyyyyyyyyyyy',
       }) || '',
   openAdUnitId: __DEV__
     ? TestIds.APP_OPEN
     : Platform.select({
-        ios: 'ca-app-pub-xxxxxxxxxxxxx/yyyyyyyyyyyyyy',
+        ios: 'ca-app-pub-3548016532531500/2414771154',
         android: 'ca-app-pub-xxxxxxxxxxxxx/yyyyyyyyyyyyyy',
       }) || '',
   nativeAdUnitId: __DEV__
     ? OtherTestIds.Image
     : Platform.select({
-        ios: 'ca-app-pub-xxxxxxxxxxxxx/yyyyyyyyyyyyyy',
+        ios: 'ca-app-pub-3548016532531500/8799112453',
         android: 'ca-app-pub-xxxxxxxxxxxxx/yyyyyyyyyyyyyy',
       }) || '',
   bannerAdUnitId: __DEV__
     ? TestIds.BANNER
     : Platform.select({
-        ios: 'ca-app-pub-xxxxxxxxxxxxx/yyyyyyyyyyyyyy',
+        ios: 'ca-app-pub-3548016532531500/3318473491',
         android: 'ca-app-pub-xxxxxxxxxxxxx/yyyyyyyyyyyyyy',
       }) || '',
   rewardedAdUnitId: __DEV__
